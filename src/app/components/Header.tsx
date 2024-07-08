9a0e7cee57f6ff4f18285314adaa723b3d13e13a
zeeshanhaider5121472 <shani721287@gmail.com>
2024-07-09 03:33:41 +0500

    leftside ui fixed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <div className="flex flex-row items-center justify-between pb-4 pl-4">
       <div className="flex flex-row items-center">
-        <BsGrid3X3GapFill className="mr-3 cursor-pointer" size={25} />
-        <p className="font-bold text-2xl mr-5 pt-1.5 text-purple-800">BPMS</p>
+        <BsGrid3X3GapFill className="mr-3 cursor-pointer" size={18} />
+        <p className="font-bold text-xl mr-5 pt-1.5 text-purple-800">BPMS</p>
       </div>
 
       <div className="flex flex-row items-center ">
-        <IoIosNotificationsOutline className="cursor-pointer ml-5" size={25} />
-        <CgProfile className="ml-5 cursor-pointer" size={25} />
+        <IoIosNotificationsOutline className="cursor-pointer ml-5" size={22} />
+        <CgProfile className="ml-5 cursor-pointer" size={22} />
       </div>
     </div>
   );
