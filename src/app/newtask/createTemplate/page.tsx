3df1d7f18a2a4110f8c5fff634f683eef8cded0d
zeeshanhaider5121472 <shani721287@gmail.com>
2024-07-31 20:47:14 +0500

    new task screen working with form

--- src/app/newtask/createTemplate/page.tsx
+++ src/app/newtask/createTemplate/page.tsx
@@ -1,14 +1,13 @@
 "use client";
-import { useSearchParams } from "next/navigation";
 
 export default function CreateTemplate() {
-  const searchParams = useSearchParams();
-  const arrayData = JSON.parse(searchParams.get("members") || "[]");
-  const stringData = searchParams.get("processName") || "";
+  // const searchParams = useSearchParams();
+  // const arrayData = JSON.parse(searchParams.get("members") || "[]");
+  // const stringData = searchParams.get("processName") || "";
   return (
     <main className="flex min-h-screen flex-col p-5 pt-5">
-      {...arrayData}
-      <div className="flex min-h-screen flex-col p-5 pt-5">{stringData}</div>
+      createNewTemplate
+      {/* {...arrayData} */}
     </main>
   );
 }
