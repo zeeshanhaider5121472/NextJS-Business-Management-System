167b5df184f64df02aac79a581204532451d61a1
zeeshanhaider5121472 <shani721287@gmail.com>
2024-08-03 20:12:49 +0500

    dnd test working functionally v2

--- src/app/newtask/createTemplate/page.tsx
+++ src/app/newtask/createTemplate/page.tsx
@@ -1,5 +1,6 @@
-import TestingTemplate from "@/app/components/rightsplitscreen_comp/home_comp/newtask_comp/TestingTemplate";
-import Tt2 from "@/app/components/rightsplitscreen_comp/home_comp/newtask_comp/Tt2";
+import Header from "@/app/components/Header";
+import TestingTemplate from "@/app/components/rightsplitscreen_comp/home_comp/newtask_comp/testing_dump/TestingTemplate";
+import Tt2 from "@/app/components/rightsplitscreen_comp/home_comp/newtask_comp/testing_dump/Tt2";
 
 export default function CreateTemplate() {
   //where i'm using useSesarchParams() that whole component should be enclosed in <suspence> </suspence>
@@ -8,7 +9,7 @@ export default function CreateTemplate() {
   // const stringData = searchParams.get("processName") || "";
   return (
     <main className="flex min-h-screen flex-col p-5 pt-5">
-      {/* <Header /> */}
+      <Header />
       {/* <Dropable /> */}
 
       {/* createNewTemplate */}
